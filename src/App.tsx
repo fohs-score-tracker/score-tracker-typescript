36fc6e4b58a408eab57c49711c11c6526d244e08
Wesley Henson <wesleyhenson2021@gmail.com>
2022-01-01 00:30:21 -0600

    refactor WelcomeScreen
    console.info('not my problem')

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,61 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, FC } from "react";
 import { API, APIContext, call } from "./api";
 import "./styles.sass";
 import LoginScreen from "./screens/LoginScreen";
 import { Routes, HashRouter, Route } from "react-router-dom";
-import { WelcomeScreen } from "./screens/WelcomeScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
 
+interface IState {
+  api: API;
+}
 
 // interface IState {
 //   api: API;
 // }
 export default function App() {
-    
-  const [players, setPlayers] = useState([])
+  const [players, setPlayers] = useState([]);
 
   // use Effect to getPlayers from Server
   useEffect(() => {
-    const getPlayers =async () => {
-      const playersFromServer = await fetchPlayers()
-      setPlayers(playersFromServer)    
-    }
-    getPlayers()
-  },[])
+    const getPlayers = async () => {
+      const playersFromServer = await fetchPlayers();
+      setPlayers(playersFromServer);
+    };
+    getPlayers();
+  }, []);
 
   // Fetch Players from API
-  const fetchPlayers = async() => {
-    const res = await fetch('https://fohs-score-tracker.herokuapp.com/players')
-    const data = await res.json()
+  const fetchPlayers = async () => {
+    const res = await fetch("https://fohs-score-tracker.herokuapp.com/players");
+    const data = await res.json();
     // print to console
-    console.log(data)
-    return data
-  }
+    console.log(data);
+    return data;
+  };
 
-
-
-  // const [state, setState] = useState<IState>({
-  //   api: {
-  //     call,
-  //     base: "https://fohs-score-tracker.herokuapp.com",
-  //     token: undefined,
-  //     setBase: (base) => setState({ ...state, api: { ...state.api, base } }),
-  //     setToken: (token) => setState({ ...state, api: { ...state.api, token } }),
-  //   },
-  // });
+  const [state, setState] = useState<IState>({
+    api: {
+      call,
+      base: "https://fohs-score-tracker.herokuapp.com",
+      token: undefined,
+      setBase: (base) => {
+        setState({ api: { ...state.api, base } });
+      },
+      setToken: (token) => {
+        setState({ api: { ...state.api, token } });
+      },
+    },
+  });
   return (
-    <WelcomeScreen players={players}/>
-    // <APIContext.Provider value={state.api}>
-    //   <HashRouter>
-    //     <Routes>
-    //       <Route path="/">
-    //         <Route index element={<LoginScreen />} />
-    //         <Route path="login" element={<LoginScreen />} />
-    //       </Route>
-    //     </Routes>
-    //   </HashRouter>
-    // </APIContext.Provider>
+    <APIContext.Provider value={state.api}>
+      <HashRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<WelcomeScreen players={players} />} />
+            <Route path="login" element={<LoginScreen />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </APIContext.Provider>
   );
-};
-
+}
--- src/components/Player.jsx
+++
@@ -1,9 +0,0 @@
-
-export const Player = ({player}) => {
-    return (
-        <div>
-            <h4>This is a player:</h4>
-            <h3>{player.name}</h3>
-        </div>
-    )
-}
